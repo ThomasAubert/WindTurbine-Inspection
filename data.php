d1f3ecc198d66f2083fbb63df6cde8961f716b70
ThomasAubert <taubert.dev@gmail.com>
2020-11-24 01:52:48 +0000

    style correction xl screens

--- data.php
+++ data.php
@@ -8,7 +8,7 @@ $inspectedItems = range(1, 100); // without AJAX
 foreach ($inspectedItems as $item) {
   //  multiples of 3 && 5
   if ($item % 15 == 0) { ?>
-    <div class=" col-md-4 col-lg-3 col-xl-2">
+    <div class=" col-md-4 col-lg-3">
       <div class="card bg-danger mb-4 border-white shadow-sm" style="height: 120px">
         <div class="card-body text-center">
           <p class="card-title font-weight-bold text-white text-uppercase">
@@ -21,7 +21,7 @@ foreach ($inspectedItems as $item) {
   <?php
     // multiples of 3
   } elseif ($item % 3 == 0) { ?>
-    <div class=" col-md-4 col-lg-3 col-xl-2">
+    <div class=" col-md-4 col-lg-3">
       <div class="card bg-warning mb-4 border-0 shadow-sm" style="height: 100px">
         <div class="card-body text-center">
           <p class="card-title font-weight-bold text-uppercase">
@@ -34,7 +34,7 @@ foreach ($inspectedItems as $item) {
   <?php
     // multiples of 5
   } elseif ($item % 5 == 0) { ?>
-    <div class=" col-md-4 col-lg-3 col-xl-2">
+    <div class=" col-md-4 col-lg-3">
       <div class="card bg-warning mb-4 border-0 shadow-sm align-text-bottom" style="height: 100px">
         <div class="card-body text-center">
           <p class="card-title font-weight-bold align-text-bottom text-uppercase">
@@ -46,7 +46,7 @@ foreach ($inspectedItems as $item) {
 
   <?php
   } else { ?>
-    <div class=" col-md-4 col-lg-3 col-xl-2">
+    <div class=" col-md-4 col-lg-3">
       <div class='card bg-dark mb-4 border-0 shadow-sm' style="height: 100px">
         <div class="card-body text-white">
           <p class='card-title font-weight-bold'><?php echo "$item"; ?></p>
