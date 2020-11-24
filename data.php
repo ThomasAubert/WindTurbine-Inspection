f67491204eb527bcf75d19f45ad6db306f44a49d
ThomasAubert <taubert.dev@gmail.com>
2020-11-24 01:50:14 +0000

    ajax script

--- data.php
+++ data.php
@@ -1,5 +1,59 @@
+<!-- PHP ARRAY ITERATION -->
 <?php
-// create the array of 100 $inspected items
-$inspectedItems = range(1, 100);
-// JSON return for ajax
-$jsonArray = json_encode($inspectedItems);
+
+$coatingDamage = 'Coating Damage';
+$lightningStrike = 'Lightning Strike';
+
+$inspectedItems = range(1, 100); // without AJAX
+foreach ($inspectedItems as $item) {
+  //  multiples of 3 && 5
+  if ($item % 15 == 0) { ?>
+    <div class=" col-md-4 col-lg-3 col-xl-2">
+      <div class="card bg-danger mb-4 border-white shadow-sm" style="height: 120px">
+        <div class="card-body text-center">
+          <p class="card-title font-weight-bold text-white text-uppercase">
+            <?php echo "$coatingDamage and $lightningStrike"; ?>
+          </p>
+        </div>
+      </div>
+    </div>
+
+  <?php
+    // multiples of 3
+  } elseif ($item % 3 == 0) { ?>
+    <div class=" col-md-4 col-lg-3 col-xl-2">
+      <div class="card bg-warning mb-4 border-0 shadow-sm" style="height: 100px">
+        <div class="card-body text-center">
+          <p class="card-title font-weight-bold text-uppercase">
+            <?php echo "$coatingDamage"; ?>
+          </p>
+        </div>
+      </div>
+    </div>
+
+  <?php
+    // multiples of 5
+  } elseif ($item % 5 == 0) { ?>
+    <div class=" col-md-4 col-lg-3 col-xl-2">
+      <div class="card bg-warning mb-4 border-0 shadow-sm align-text-bottom" style="height: 100px">
+        <div class="card-body text-center">
+          <p class="card-title font-weight-bold align-text-bottom text-uppercase">
+            <?php echo "$lightningStrike"; ?>
+          </p>
+        </div>
+      </div>
+    </div>
+
+  <?php
+  } else { ?>
+    <div class=" col-md-4 col-lg-3 col-xl-2">
+      <div class='card bg-dark mb-4 border-0 shadow-sm' style="height: 100px">
+        <div class="card-body text-white">
+          <p class='card-title font-weight-bold'><?php echo "$item"; ?></p>
+        </div>
+      </div>
+    </div>
+<?php
+  }
+} ?>
+<!-- End of PHP Iteration -->
--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-
-// wind turbine items using json file from data.php
-// $inspectedItems = json_decode(stripslashes($_POST['$inspectedItems']));
-
-// iteration through the items
-
-$coatingDamage = 'Coating Damage';
-$lightningStrike = 'Lightning Strike';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,63 +13,8 @@ $lightningStrike = 'Lightning Strike';
       <h1 class="text-dark text-center font-weight-bold my-5 shadow rounded-lg py-1">Wind Turbine Inspection Items</h1>
 
       <!-- Grid Container -->
-      <div class="row mx-auto">
+      <div id='container' class="row mx-auto">
 
-        <!-- PHP ARRAY ITERATION -->
-        <?php
-        $inspectedItems = range(1, 100); // without AJAX
-        foreach ($inspectedItems as $item) {
-          //  multiples of 3 && 5
-          if ($item % 15 == 0) { ?>
-            <div class=" col-md-4 col-lg-3 col-xl-2">
-              <div class="card bg-danger mb-4 border-white shadow-sm" style="height: 120px">
-                <div class="card-body text-center">
-                  <p class="card-title font-weight-bold text-white text-uppercase">
-                    <?php echo "$coatingDamage and $lightningStrike"; ?>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-          <?php
-            // multiples of 3
-          } elseif ($item % 3 == 0) { ?>
-            <div class=" col-md-4 col-lg-3 col-xl-2">
-              <div class="card bg-warning mb-4 border-0 shadow-sm" style="height: 100px">
-                <div class="card-body text-center">
-                  <p class="card-title font-weight-bold text-uppercase">
-                    <?php echo "$coatingDamage"; ?>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-          <?php
-            // multiples of 5
-          } elseif ($item % 5 == 0) { ?>
-            <div class=" col-md-4 col-lg-3 col-xl-2">
-              <div class="card bg-warning mb-4 border-0 shadow-sm align-text-bottom" style="height: 100px">
-                <div class="card-body text-center">
-                  <p class="card-title font-weight-bold align-text-bottom text-uppercase">
-                    <?php echo "$lightningStrike"; ?>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-          <?php
-          } else { ?>
-            <div class=" col-md-4 col-lg-3 col-xl-2">
-              <div class='card bg-dark mb-4 border-0 shadow-sm' style="height: 100px">
-                <div class="card-body text-white">
-                  <p class='card-title font-weight-bold'><?php echo "$item"; ?></p>
-                </div>
-              </div>
-            </div>
-        <?php
-          }
-        } ?>
-        <!-- End of PHP Iteration -->
 
       </div> <!-- end of grid -->
 
@@ -92,7 +26,16 @@ $lightningStrike = 'Lightning Strike';
 <!-- Footer -->
 <?php include('./templates/footer.php'); ?>
 
-
+<!-- Ajax script to get the PHP from data.php -->
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#container").load("data.php", function(res, status, xhr) {
+      if (status == 'error') {
+        alert('There was an error with the Ajax call')
+      }
+    });
+  });
+</script>
 </body>
 
 </html>
